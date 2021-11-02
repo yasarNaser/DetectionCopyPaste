e95659426706ec231b934e3a8dfd6469793e4807
YasarN <yasar.naser@exalt.email>
2021-11-02 10:46:13 +0200

    iframe

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,29 +1,28 @@
 describe('Test Copy Paste Detection', () => {
     beforeEach(() => {
-        cy.visit('/')
-      });
-      
+        cy.visit('http://localhost:5000/iframePage')
+        cy.get('#myFrame').its('0.contentDocument').as('document')
+        cy.get('#myFrame').its('0.contentDocument.body').then(cy.wrap).as('body')
+    });
+
     it('detect copypaste in email', () => {
-        cy.get("[type='email']").focus()
-        cy.document().then((doc) => {
+        cy.get('@body').find("[type='email']").focus()
+        cy.get('@document').then((doc) => {
             doc.execCommand("copy")
         })
-        cy.get('#emailDetect').should('include.text', 'Copy is detected in email')
-
-        cy.document().then((doc) => {
+        cy.get('@body').find('#emailDetect').should('include.text', 'Copy is detected in email')
+        
+        cy.get('@document').then((doc) => {
             doc.execCommand("paste")
         })
-        cy.get('#emailDetect').should('include.text', 'Paste is detected in email')
-
+        cy.get('@body').find('#emailDetect').should('include.text', 'Paste is detected in email')
     })
     it('detect paste in password', () => {
-        cy.get("[type='password']").focus()
-
-        cy.document().then((doc) => {
+        cy.get('@body').find("[type='password']").focus()
+        cy.get('@document').then((doc) => {
             doc.execCommand("paste")
         })
-        cy.get('#passwordDetect').should('include.text', 'Paste is detected in password')
-
+        cy.get('@body').find('#passwordDetect').should('include.text', 'Paste is detected in password')
     })
 
 })
